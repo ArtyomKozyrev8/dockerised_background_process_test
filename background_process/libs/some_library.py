3df2f42e437dc8453099f3b6028ca97649f2ddf2
Artyom Kozyrev <43035134+ArtyomKozyrev1989@users.noreply.github.com>
2020-06-30 19:50:52 +0300

    added logging

--- background_process/libs/some_library.py
+++ background_process/libs/some_library.py
@@ -1,6 +1,8 @@
 import time
+import logging
 
 
 def do_some_work():
     print("Do some work here")
+    logging.info("Do some work here")
     time.sleep(1)
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from background_process.libs import some_library
+import logging
+
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
 if __name__ == '__main__':
     while True:
