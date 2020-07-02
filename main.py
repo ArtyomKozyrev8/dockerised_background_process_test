ff09ca2b53907b3d9f586df2ee80fa941fb2f4cf
Artyom Kozyrev <43035134+ArtyomKozyrev1989@users.noreply.github.com>
2020-07-02 22:58:27 +0300

    file cr

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from background_process.libs import some_library
 import logging
+import time
 
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO, filename="logs/logfile.txt")
 
@@ -7,3 +8,6 @@ if __name__ == '__main__':
     while True:
         some_library.do_some_work()
         some_library.extended_do_work()
+        with open("logs/app.txt", mode='a') as f:
+            f.write("xyz\n")
+        time.sleep(1)
