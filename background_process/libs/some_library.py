e2a85b363ea5d277d7d8faf94f4287f0c079bd29
Artyom Kozyrev <43035134+ArtyomKozyrev1989@users.noreply.github.com>
2020-07-02 10:57:54 +0300

    extended lib

--- background_process/libs/some_library.py
+++ background_process/libs/some_library.py
@@ -6,3 +6,9 @@ def do_some_work():
     print("Do some work here")
     logging.info("Do some work here")
     time.sleep(1)
+
+
+def extended_do_work():
+    print("Extended do some work here")
+    logging.info("Extended do some work here!!!")
+    time.sleep(1)
--- main.py
+++ main.py
@@ -6,3 +6,4 @@ logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 if __name__ == '__main__':
     while True:
         some_library.do_some_work()
+        some_library.extended_do_work()
